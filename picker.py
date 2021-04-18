f588b54a6da1aab5d35cfa1a83fbe34f83d369ea
tuffinmuffin <tuffinmuffin@gmail.com>
2021-04-18 15:12:56 -0600

    working version before refactor

--- picker.py
+++ picker.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import sys
 import numpy as np
-
+import math
 
 PICKER = "Color Picker"
 CANVAS = "Canvas"
@@ -22,15 +22,34 @@ def loadPalette(size = SIZE):
     palette = cv.imread("wheel1.jpg")
     return cv.resize(palette, size)
 
-### Your code here
+# Mouse callback function
+def mouseCallbackFunction(event, x, y, flags, param):
+    palette = param[0]
+    canvas = param[1]
+    if(not event == cv.EVENT_LBUTTONDOWN):
+        return
+
+    color0 = findColor(palette, x, y)
+    color1 = findColor(canvas, x, y)
+    mixed = mixColors(color0, color1)
+    canvas[:][:] = mixed
+    displayImage(canvas, CANVAS)
+
+# Find the color for an image at the given location
+def findColor(img,x,y):
+    return img[x][y]
+
+# Mix two colors
+def mixColors(color1, color2):
+    mix = color1*0.5 + color2*0.5
+    return np.ceil(mix)
 
 def main():
     canvas = createCanvas((255,255,255))
     palette = loadPalette()
-    print(canvas[1][1])
-    print(palette[1][1])
     displayImage(palette, PICKER)
     displayImage(canvas, CANVAS)
+    cv.setMouseCallback(PICKER, mouseCallbackFunction, (palette, canvas))
     cv.waitKey(0)
 
 
